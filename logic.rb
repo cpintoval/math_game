fdf4647ef69bf435d299ae06bc7ff7f7e318023a
cpintoval <cpintovalderrama@gmail.com>
2015-08-27 22:53:25 +0000

    Question generation working with random numbers and operations

---
+++ logic.rb
@@ -0,0 +1,51 @@
+# This file contains the actual logic of the Math Game
+
+
+# Initializing player's lives
+@players = [
+  {
+    id: 1,
+    name: "Player 1",
+    lives: 3,
+    score: 0
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    lives: 3,
+    score: 0
+  }
+]
+@actual_question = ""
+@actual_answer = nil
+@actual_player = 1
+
+def generate_question
+  operation = Random.rand(1..4)
+  num1 = Random.rand(1..20)
+  num2 = Random.rand(1..20)
+  case operation
+  when 1
+    # Addition
+    @actual_answer = num1 + num2
+    "What does #{num1} plus #{num2} equal?"
+  when 2
+    # Substraction
+    @actual_answer = num1 - num2
+    "What does #{num1} minus #{num2} equal?"
+  when 3
+    # Multiplication
+    @actual_answer = num1 * num2
+    "What does #{num1} times #{num2} equal?"
+  when 4
+    # Division
+    @actual_answer = (num1 / num2).round(1)
+    "What does #{num1} divided by #{num2} equal (use one decimal place if needed)?"
+  end
+end
+
+def verify_answer(answer)
+  @players[@actual_player - 1][:score] += @actual_answer == answer ? 1 : 0
+  @players[@actual_player - 1][:lives] -= @actual_answer == answer ? 0 : 1
+  @actual_answer == answer
+end
---
+++ main.rb
@@ -0,0 +1,7 @@
+# This is the main entrypoint into the program
+# It requires the other files/gems that it needs
+
+require 'colorize'
+require './logic'
+
+puts generate_question
